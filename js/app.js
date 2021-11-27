ef18cfc944cd47a00bc8071a1126eb13b504393e
Samar Ghilan <snl.410@hotmail.com>
2021-11-27 10:41:23 +0400

    تعريب و تعديل التصميم

--- js/app.js
+++ js/app.js
@@ -8,14 +8,6 @@ const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
 
-footer_input.addEventListener("focus", () => {
-  footer_input.classList.add("focus");
-});
-
-footer_input.addEventListener("blur", () => {
-  if (footer_input.value != "") return;
-  footer_input.classList.remove("focus");
-});
 
 function closeMenu() {
   navbar.classList.remove("open");
@@ -98,4 +90,13 @@ var mySwiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
     nextEl: ".swiper-button-next",
   },
+});
+ 
+footer_input.addEventListener("focus", () => {
+ footer_input.classList.add("focus");
+});
+
+footer_input.addEventListener("blur", () => {
+  if (footer_input.value != "") return;
+  footer_input.classList.remove("focus");
 });
