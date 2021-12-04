a005e4773cbd5c9ba57123cf8e9ae315aaecad21
Samar Ghilan <snl.410@hotmail.com>
2021-12-04 15:19:48 +0400

    try fire base

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+let firebaseConfig = {
+    // Enter your firebase credentials
+    apiKey: "AIzaSyD2_sEN36KJl35RErHUa-W6DW7TK0kBnSk",
+    authDomain: "my-website-113ed.firebaseapp.com",
+    projectId: "my-website-113ed",
+    storageBucket: "my-website-113ed.appspot.com",
+    messagingSenderId: "766550759023",
+    appId: "1:766550759023:web:fd1bc7a57e12365406df9a"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+
+
+   
